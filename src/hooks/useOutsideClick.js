1279fb888d12d08c0dc7f1dcadbd67f0f3e13982
Abhishek <abhishekparmar162@gmail.com>
2023-09-28 18:19:44 +0530

    added custom hook to close the modal

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+
+const useOutsideClick = (handler, listenCapture = true) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        console.log("clicked");
+        handler();
+      }
+    };
+    document.addEventListener("click", handleClick, listenCapture);
+    return () =>
+      document.removeEventListener("click", handleClick, listenCapture);
+  }, [handler, listenCapture]);
+  return ref;
+};
+
+export default useOutsideClick;
