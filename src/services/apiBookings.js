d8822f414198ff60fbfbd4c754c95019324b2e68
Abhishek <abhishekparmar162@gmail.com>
2023-10-02 16:29:21 +0530

    new changes

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -2,14 +2,18 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data: bookings, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
-    // .select("*,cabins(name),guests(fullName,email)");
     .select(
       "id,created_at, startDate,endDate,numOfGuests,status,totalPrice, cabins(name),guests(fullName,email)"
     );
-  // ();
+
+  //filter
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+
+  const { data: bookings, error } = await query;
 
   if (error) {
     console.log(error);
